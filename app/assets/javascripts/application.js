106384f4cf335934bcc27b979669658cf92c85da
filomenosantos <filomenosantos@gmail.com>
2015-01-13 02:28:45 +0100

    Staff working

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,12 @@
 //= require_tree .
 
 function remove_fields(link){
-	$(link).previous("input[type=hidden]").value = 1;
-	$(link).up(".p_fields").hidde();
+    $(link).prev("input[type=hidden]").val("1");
+    $(link).closest(".fields").hide();
+}
+
+function add_fields(link, association, content){
+    var new_id = new Date().getTime();
+    var regexp = new RegExp("new_" + association, "g");
+    $(link).before(content.replace(regexp, new_id));
 }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def link_to_remove_fields(name, f)
+    f.hidden_field(:_destroy) + link_to("remove", '#', onclick: "remove_fields(this)")
+  end
+  
+  def link_to_add_fields(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s + "/" + association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to(name, '#', onclick: "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+  end
 end
